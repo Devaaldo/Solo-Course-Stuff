f9aea1ff10a93bda68a4b7bebfb4c6508b3395ef
Devaaldo <akbaraja551@gmail.com>
2024-03-31 13:18:08 +0700

    js add map,array,dll

---
+++ JavaSript Dicoding/kuis_map.js
@@ -0,0 +1,8 @@
+const currency = new Map([
+	["USD", 14000],
+    ["JPY", 131],
+    ["SGD", 11000],
+	["MYR", 3500]
+]);
+const priceInIDR = currency.get("JPY") * priceInJPY
+console.log(priceInIDR)
